8c8ae062f8dabde135af78a9cb531e97b0d5b430
Stanley Nzegwu <nzegwustanley@gmail.com>
2024-02-11 11:58:34 +0100

    coded faq

--- src/components/Section_four.jsx
+++ src/components/Section_four.jsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
+import { FAQ } from "../constants";
+
 const Section_four = () => {
+  const [expanded, setExpanded] = useState([]);
+
+  function handleExpansion(index) {
+    expanded.includes(index)
+      ? setExpanded(expanded.filter((val) => val !== index))
+      : setExpanded(expanded.concat(index));
+  }
+
   return (
-    <div className="relative h-screen w-full" id="section_four">
-      section four
+    <div className=" relative px-4 md:px-8 mt-8 h-screen w-full z-20 bg-white" id="section_four">
+      <span className="block uppercase text-lg font-bold">faq</span>
+      <div className="flex flex-col gap-4">
+        {FAQ.map(({ question, answer }, index) => (
+          <div
+            key={index}
+            className="bg-gray-100 px-4 rounded-t-lg cursor-pointer rounded-b-lg"
+            onClick={() => handleExpansion(index)}
+          >
+            <div className="py-4 flex justify-between ">
+              <p className="font-semibold text-lg">{question}</p>
+              <span className="text-xl font-bold ">{expanded.includes(index) ? "-" : "+"}</span>
+            </div>
+            <p
+              className={`${
+                expanded.includes(index) ? "" : "hidden"
+              } text-sm py-4 border-t rounded-b-lg`}
+            >
+              {answer}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
