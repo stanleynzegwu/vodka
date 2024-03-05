8d8549b766410695b1aa99fadb1741c4e1e8589a
Stanley Nzegwu <nzegwustanley@gmail.com>
2024-03-05 11:12:06 +0100

    new commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,19 @@
+import cart from "/images/shopping-cart.png";
+import logo from "/images/silverLogo.png";
+
 const Navbar = () => {
-  return <div className={`relative h-20 w-full bg-red-100 z-20`}>nav</div>;
+  return (
+    <div className={`fixed h-16 w-full bg-[#1a1919] z-50`}>
+      <ul className="flex items-center justify-between h-full">
+        <li className="w-[150px] h-full flex items-center justify-center">
+          <img className=" " src={logo} alt="logo" />
+        </li>
+        <li className="w-[150px] h-full flex justify-center items-center">
+          <img className="w-[30px] h-[30px]" src={cart} alt="shopping-cart" />
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
